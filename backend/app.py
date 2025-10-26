edf32c6b699978b0cee3b45ce48da704d8eab811
wilsonz3-dev <177430156+wilsonz347@users.noreply.github.com>
2025-10-25 23:20:41 -0400

    Added predictor

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,53 @@
+import sys
+import os
+import json
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent  # backend/
+ROOT_DIR = BASE_DIR.parent  # root/
+sys.path.insert(0, str(ROOT_DIR))
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from model.input_preds import DiseasePredictor
+import ML_files.src.config as Config
+
+# backend/model/model.pth
+model_path = os.path.join(BASE_DIR, "model", "model.pth")
+
+'''
 import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from model.input_preds import DiseasePredictor
+import ML_files.src.config as Config
+import os
 
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "model.pth")
+'''
 app = Flask(__name__)
 CORS(app)
 
+# Initialize the predictor
+predictor = DiseasePredictor(
+    model_path=model_path,
+    training_data_path=Config.Config.PROCESSED_TRAIN_PATH
+)
+
 @app.route('/predict', methods = ['POST'])
 def prediction():
-    data = request.get_json()
-    symptom_ids = data.get('symptom_ids', [])
-    print("Received symptom IDs:", symptom_ids)
-    print("Number of symptoms:", len(symptom_ids))
-    
-    # Send back a dummy response to test
-    dummy_predictions = [
-        {"disease": "Test Disease 1", "confidence": 0.75},
-        {"disease": "Test Disease 2", "confidence": 0.15},
-        {"disease": "Test Disease 3", "confidence": 0.10}
-    ]
-    
-    return jsonify({'predictions': dummy_predictions})
-    #predictions = model.predict(symptom_ids)
+    try:
+        data = request.get_json()
+        symptoms = data.get('symptom_ids', [])
 
-    #raise jsonify({'predictions':predictions})
+        predictions = predictor.get_top_predictions(symptoms, top_k=3)
+            
+        return jsonify({'predictions': predictions}), 200
+    
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
